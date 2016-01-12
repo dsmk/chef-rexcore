51cef1d064265a72d40b445d0c97838051cd63bc
David King <david@rexden.us>
2016-01-12 17:04:45 -0500

    Re-add the sudoers.docker file

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'you@example.com'
 license 'all_rights'
 description 'Installs/Configures rexcore'
 long_description 'Installs/Configures rexcore'
-version '0.1.1'
+version '0.1.2'
 
 depends 'chocolatey'
 depends 'chef-client'
