63cf734cce51e39395331bf2c8624ec684d9bd27
David King <david@rexden.us>
2016-01-13 08:32:01 -0500

    Adding web core recipe (first version)

---
+++ recipes/web.rb
@@ -0,0 +1,18 @@
+case node[:os]
+when 'linux'
+  include_recipe 'rexcore::_web_unix'
+when 'windows'
+  log "not yet implemented"
+end
+
+# common controls no matter where the web server comes from
+#
+control_group "Validate web services" do
+  control "Ensure that the web server is listening on port 80" do
+    it "listening on port 80" do
+      expect(port(80)).to be_listening
+    end
+  end
+end
+
+# vi: expandtab ts=2 
